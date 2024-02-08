b4553645d15fc5574ba76b631d1672278b454a66
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2024-02-08 08:14:10 +0100

    Zadanie domowe 4

--- ChallengeApp2024/ChallengeApp2024/Program.cs
+++ ChallengeApp2024/ChallengeApp2024/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Zenek";
+var sex = 'M';
+var age = 17;
+
+if (age < 30)
+{
+    if (sex == 'F')
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
+else if (age == 30 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+
+
