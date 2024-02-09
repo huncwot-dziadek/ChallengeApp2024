a885266597dcd7a4306dea19011e35c1199c4555
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2024-02-09 09:56:48 +0100

    Lesson 6

--- ChallengeApp2024/ChallengeApp2024/Program.cs
+++ ChallengeApp2024/ChallengeApp2024/Program.cs
@@ -1,22 +1,16 @@
-﻿var name = "Zenek";
-var sex = 'M';
-var age = 17;
+﻿using ChallengeApp2024;
 
-if (age < 30)
-{
-    if (sex == 'F')
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-}
-else if (age == 30 && name == "Ewa")
-{
-    Console.WriteLine("Ewa, lat 30");
-}
+User user1 = new User("Dupek");
+User user2 = new User();
+User user3 = new User("Debilka", "12345678");
+User user4 = new User("Kretynka");
 
 
+user1.AddScore(7);
+user1.AddScore(17);
+
+var result = user1.Result;
+
+Console.WriteLine(result);
+Console.WriteLine(User.GameName);
 
