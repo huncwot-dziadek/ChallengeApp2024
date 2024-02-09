0cc730a62f04a98a9b734be597378401fafd1607
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2024-02-09 12:33:40 +0100

    Zadanie 6

---
+++ ChallengeApp2024/ChallengeApp2024/Employee.cs
@@ -0,0 +1,37 @@
+﻿namespace ChallengeApp2024
+{
+    public class Employee
+    {
+        private List<int> gradeList = new List<int>();
+
+        
+
+        public Employee(string name, string surname, int age) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set;}
+
+        public int gradeSum
+        {
+            get
+            {
+                return gradeList.Sum();
+            }
+        }
+
+
+        public void AddGradeList(int grade) 
+        {
+            gradeList.Add(grade);
+        }
+
+
+
+
+    }
+}
--- ChallengeApp2024/ChallengeApp2024/Program.cs
+++ ChallengeApp2024/ChallengeApp2024/Program.cs
@@ -1,16 +1,39 @@
 ﻿using ChallengeApp2024;
 
-User user1 = new User("Dupek");
-User user2 = new User();
-User user3 = new User("Debilka", "12345678");
-User user4 = new User("Kretynka");
+List<int> sumsEmployeesList = new List<int>();
+
+Employee empArnold = new Employee("Arnold", "Szwarc", 26);
+Employee empMarianna = new Employee("Marianna", "Niepoprawna", 33);
+Employee empZenobiusz = new Employee("Zenobiusz", "Spokrewniony", 42);
+
+empArnold.AddGradeList(5);
+empArnold.AddGradeList(7);
+empArnold.AddGradeList(6);
+empArnold.AddGradeList(3);
+empArnold.AddGradeList(7);
+sumsEmployeesList.Add(empArnold.gradeSum);
 
 
-user1.AddScore(7);
-user1.AddScore(17);
+empMarianna.AddGradeList(7);
+empMarianna.AddGradeList(8);
+empMarianna.AddGradeList(8);
+empMarianna.AddGradeList(6);
+empMarianna.AddGradeList(9);
+sumsEmployeesList.Add(empMarianna.gradeSum);
 
-var result = user1.Result;
 
-Console.WriteLine(result);
-Console.WriteLine(User.GameName);
+empZenobiusz.AddGradeList(2);
+empZenobiusz.AddGradeList(3);
+empZenobiusz.AddGradeList(1);
+empZenobiusz.AddGradeList(7);
+empZenobiusz.AddGradeList(5);
+sumsEmployeesList.Add(empZenobiusz.gradeSum);
+
+sumsEmployeesList.Sort();
+
+foreach (int num in sumsEmployeesList)
+{
+    Console.WriteLine(num);
+}
+
 
