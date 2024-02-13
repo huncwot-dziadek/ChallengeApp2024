4246b3fcac368fe9ea5cc8e49c0ed92f47b03589
Krzysztof <krzysztofnalewajko59@gmail.com>
2024-02-13 04:07:36 +0100

    Zadanie nr 9

--- ChallengeApp2024/ChallengeApp2024/Lesson9/Lesson9.Tests/StatisticsTests.cs
+++ ChallengeApp2024/ChallengeApp2024/Lesson9/Lesson9.Tests/StatisticsTests.cs
@@ -4,31 +4,31 @@ namespace Lesson9.Tests
     {
 
         [Test]
-        public void DoSatisticsFindTheNumberOfMax()
+        public void WhetherTheSatisticsFindTheRangeOfMaxCorrectly()
         {
             Employee employee = new Employee("Konrad", "Wagabunda");
             employee.AdGrade(3);
             employee.AdGrade(5);
-            employee.AdGrade(-6);
+            employee.AdGrade(6);
             var statistics = employee.GetStatistics();
 
-            Assert.AreEqual(5, statistics.Max);
+            Assert.AreEqual(6, statistics.Max);
         }
 
         [Test]
-        public void DoSatisticsFindTheNumberOfMin()
+        public void WhetherTheSatisticsFindTheRangeOfMinCorrectly()
         {
             Employee employee = new Employee("Konrad", "Wagabunda");
             employee.AdGrade(3);
             employee.AdGrade(5);
-            employee.AdGrade(-6);
+            employee.AdGrade(6);
             var statistics = employee.GetStatistics();
 
-            Assert.AreEqual(-6, statistics.Min);
+            Assert.AreEqual(3, statistics.Min);
         }
 
         [Test]
-        public void WetherSatisticsCalculateTheAverageCorrectly()
+        public void WetherTheSatisticsCalculateTheAverageCorrectly()
         {
             Employee employee = new Employee("Konrad", "Wagabunda");
             employee.AdGrade(3);
