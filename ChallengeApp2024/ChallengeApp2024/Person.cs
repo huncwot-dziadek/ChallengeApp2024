27782e217fff9de6d9fde5b7193dcdcca718e173
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2024-02-16 07:51:13 +0100

    popr ost

--- ChallengeApp2024/ChallengeApp2024/Person.cs
+++ ChallengeApp2024/ChallengeApp2024/Person.cs
@@ -10,14 +10,12 @@
         }
 
         public string Name { get; private set; }
+
         public string Surname { get; private set; }
+
         public string Sex { get; private set; }
-<<<<<<< HEAD
+
         public int Age { get; private set; }
 
-        int zmienna;
-
-=======
->>>>>>> 44a4ec67e3e10e91349ed57c5b6283260163bb12
     }
 }
--- ChallengeApp2024/ChallengeApp2024/Program.cs
+++ ChallengeApp2024/ChallengeApp2024/Program.cs
@@ -9,6 +9,7 @@ Console.WriteLine();
 while (true)
 {
     Console.Write("Provide another employee evaluation:  ");
+
     var input = Console.ReadLine();
 
     if (input == "Q" || input == "q")
@@ -28,17 +29,14 @@ while (true)
 var statistics = employee.GetStatistics();
 
 Console.WriteLine();
-<<<<<<< HEAD
-Console.Write($"Max = {statistics.Max}   ");
-Console.Write($"Min = {statistics.Min}   ");
-Console.Write($"Average = {statistics.Average:N2}   ");
-Console.WriteLine($"AverageLetter = {statistics.AverageLetter}   ");
-=======
 Console.WriteLine("============================================");
 Console.WriteLine($"{employee.Name} {employee.Surname} final results:");
 Console.WriteLine($"Masimum rating = {statistics.Max}   ");
 Console.WriteLine($"Mininum rating = {statistics.Min}   ");
 Console.WriteLine($"Average rating = {statistics.Average:N2}   ");
 Console.WriteLine("============================================");
->>>>>>> 44a4ec67e3e10e91349ed57c5b6283260163bb12
+Console.Write($"Max = {statistics.Max}   ");
+Console.Write($"Min = {statistics.Min}   ");
+Console.Write($"Average = {statistics.Average:N2}   ");
+Console.WriteLine($"AverageLetter = {statistics.AverageLetter}   ");
 
